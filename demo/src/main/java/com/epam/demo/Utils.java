8dcb464cbfd8e633f10f8dfd5a4a700658a89d3c
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2024-02-29 11:05:13 +0600

    mvp

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,5 +5,12 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        for (String s:
+             args) {
+            if(com.epam.utils.StringUtils.isPositiveNumber(s)){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,10 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        if(!org.apache.commons.lang3.math.NumberUtils.isCreatable(str)) {
+            return false;
+        }
+        if(str.charAt(0) == '0' || str.charAt(0) == '-') return false;
+        return true;
     }
 }
